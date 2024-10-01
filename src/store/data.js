d51ef13e2f0ed82712eb8a2ff71b5d5221da2480
Saravanan K S <saravananks@presidio.com>
2024-10-01 12:43:32 +0530

    feat: updated mock data

--- src/store/data.js
+++ src/store/data.js
@@ -13,5 +13,30 @@ export const STOCK_LIST = [
         id: 3,
         name: 'Amazon.com Inc.',
         price: 3372.20,
+    },
+    {
+        id: 4,
+        name: 'Google LLC',
+        price: 2800.50,
+    },
+    {
+        id: 5,
+        name: 'Facebook, Inc.',
+        price: 355.64,
+    },
+    {
+        id: 6,
+        name: 'Tesla, Inc.',
+        price: 730.17,
+    },
+    {
+        id: 7,
+        name: 'Netflix, Inc.',
+        price: 590.65,
+    },
+    {
+        id: 8,
+        name: 'NVIDIA Corporation',
+        price: 220.42,
     }
 ]
